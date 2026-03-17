623d7d0c5629171e8d167f302d11e91d79b530c9
Isen Naiken <isen.naiken@nexomedis.com>
2026-03-17 17:58:32 +0100

    arangemnt du code

--- registration.py
+++ registration.py
@@ -7,21 +7,16 @@ import os
 os.environ.setdefault("CUDA_VISIBLE_DEVICES", "")
 from valis import registration
 
+# gestion des chemins
 patient = "a"
 hes_path = f"/silver/ube/slides_ome_tiff/{patient}_HES.ome.tiff"
 cd30_path = f"/silver/ube/slides_ome_tiff/{patient}_CD30.ome.tiff"
 base_results_dst_dir = "/silver/ube/registration_results_v2"
-
-os.makedirs(base_results_dst_dir, exist_ok=True)
-
-
 pair_results_dir = os.path.join(base_results_dst_dir, patient)
 os.makedirs(pair_results_dir, exist_ok=True)
+registered_slide_dst_dir = os.path.join(pair_results_dir, "registered_slides")
 
-# Use parent folder of the input slides as VALIS source directory.
 img_list = [hes_path, cd30_path]
-
-print(f"Aligning pair")
 
 registrar = registration.Valis(
     src_dir="/silver/ube/slides_ome_tiff/",
@@ -31,10 +26,11 @@ registrar = registration.Valis(
     max_processed_image_dim_px = 1600,
 )
 
+print(f"Aligning pair")
+
 rigid_registrar, non_rigid_registrar, error_df = registrar.register()
 
-registered_slide_dst_dir = os.path.join(pair_results_dir, "registered_slides")
-registrar.warp_and_save_slides(registered_slide_dst_dir)
+# registrar.warp_and_save_slides(registered_slide_dst_dir)
 
 print(f"Pair completed successfully.")
 registration.kill_jvm()
